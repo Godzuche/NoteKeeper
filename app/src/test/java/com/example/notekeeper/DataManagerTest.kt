5c021ac837d1b99169ec81f4bd071ebfb500332c
Administrator <godxuche@gmail.com>
2021-09-18 23:39:34 +0100

    added a local JVM Unit Test - DataManagerTest.kt to test if the notes are added correctly...

---
+++ app/src/test/java/com/example/notekeeper/DataManagerTest.kt
@@ -0,0 +1,20 @@
+package com.example.notekeeper
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DataManagerTest {
+
+    @Test
+    fun addNote() {
+        val course = DataManager.courses["android_async"]!!
+        val noteTitle = "This is a test note"
+        val noteText = "This is the body of my test note"
+
+        val index = DataManager.addNote(course, noteTitle, noteText)
+        val note = DataManager.notes[index]
+        assertEquals(course, note.course)
+        assertEquals(noteTitle, note.title)
+        assertEquals(noteText, note.text)
+    }
+}
