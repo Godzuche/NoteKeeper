492bb1deb930eb11fcef330ead7666b68cd856ef
Administrator <godxuche@gmail.com>
2021-09-19 00:22:58 +0100

    created more complete test...

--- app/src/androidTest/java/com/example/notekeeper/CreateNewNoteTest.kt
+++ app/src/androidTest/java/com/example/notekeeper/CreateNewNoteTest.kt
@@ -1,11 +1,13 @@
 package com.example.notekeeper
 
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.*
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,12 +20,23 @@ class CreateNewNoteTest {
 
     @Test
     fun createNewNote() {
+        val course = DataManager.courses["android_async"]
         val noteTitle = "Test note title"
         val noteText = "This is the body of our test note"
 
         onView(withId(R.id.fab)).perform(click())
 
+        onView(withId(R.id.courseSpinner)).perform(click())
+        onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
+
         onView(withId(R.id.note_title)).perform(typeText(noteTitle))
-        onView(withId(R.id.note_text)).perform(typeText(noteText))
+        onView(withId(R.id.note_text)).perform(typeText(noteText), closeSoftKeyboard())
+
+        pressBack()
+
+        val note = DataManager.notes.last()
+        assertEquals(course, note.course)
+        assertEquals(noteTitle, note.title)
+        assertEquals(noteText, note.text)
     }
 }
